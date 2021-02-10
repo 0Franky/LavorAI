30b112ccce33b54415aba0cfcb1b13ca7d7c9196
0Franky <frhaexin@gmail.com>
2021-02-10 20:58:32 +0100

    added class main and refactored LearnerWork

---
+++ __main.py
@@ -0,0 +1,17 @@
+from WorkUtils import workDecoder
+import LearnerWork
+
+
+dataset = LearnerWork.load_dataset()
+# print(dataset)
+
+# model = LearnerWork.KNearestNeighbourClassifier(dataset)
+# model = LearnerWork.GaussianNaiveBayes(dataset)
+model = LearnerWork.DecisionTree(dataset)
+
+res = workDecoder(model.predict([[2,2,2,5,4,2,2,5,2,4]]))
+
+if res[0] != "Errore!" :
+    print("Secondo me il tuo lavoro è di tipo '" + res[1] + "'... Ti vedo bene come " + res[0])
+else:
+    print("Si è verificato un errore. Scusa ma sto ancora imparando... '.'")
