5f5471e48b127333ff9b58b3134a05ba9362eba4
0Franky <frhaexin@gmail.com>
2021-02-15 19:48:52 +0100

    update

--- __main.py
+++ __main.py
@@ -15,21 +15,25 @@ model = LearnerWork.KNearestNeighbourClassifier(dataset)
 # model = LearnerWork.DecisionTree(dataset, max_depth = 3)
 # model = LearnerWork.DecisionTree(dataset, max_depth = 8)
 
-risposte = []
-risposte.append(startSurvey())
+# models = [LearnerWork.KNearestNeighbourClassifier, LearnerWork.GaussianNaiveBayes, LearnerWork.DecisionTree]
+
+risposte = [startSurvey()]
 # print(model.decision_path(risposte))
 res = workDecoder(model.predict(risposte))
 
 print('\n\n\n')
 if res[0] != "Errore!":
-    print("Secondo me il tuo lavoro e' di tipo '" +
-          res[1] + "'... Ti vedo bene come " + res[0])
+  print("Secondo me il tuo lavoro e' di tipo '" +
+        res[1] + "'... Ti vedo bene come " + res[0])
 else:
-    print("Si è verificato un errore. Scusa ma sto ancora imparando... '.'")
+  print("Si è verificato un errore. Scusa ma sto ancora imparando... '.'")
 
 
 print('\n\n')
 
 # # Test
-# cm.show_matrix([1, 14, 5, 4, 5, 3, 3, 5, 6, 5], [2, 3, 4, 7, 3, 4, 2, 3, 6, 7], getLabels())
-vu.validateDataset(dataset, 5, LearnerWork.DecisionTree)
+# for m in models:
+#   # cm.show_matrix([1, 14, 5, 4, 5, 3, 3, 5, 6, 5], [2, 3, 4, 7, 3, 4, 2, 3, 6, 7], getLabels())
+#   vu.validateDataset(dataset, 5, m)
+
+# # vu.validateDataset(dataset, 5, LearnerWork.DecisionTree)
